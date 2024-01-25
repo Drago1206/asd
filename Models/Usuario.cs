60bf1416726a507a73bcce27f3ccfead8a0e1729
sgelvez <sgelvez>
2024-01-25 11:08:39 -0500

    Se arreglo la configuracion del webService

---
+++ Models/Usuario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WcfPedidos30.Models
+{
+    public class Usuario
+    {
+        /// <summary>
+        /// Nombre del usuario
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// Contraseña del usuario
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfPruebas30
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+    // NOTE: para iniciar el Cliente de prueba WCF para probar este servicio, seleccione Service1.svc o Service1.svc.cs en el Explorador de soluciones e inicie la depuración.
+    public class Service1 : IService1
+    {
+        [return: MessageParameter(Name = "Producto")]
+        public RespProducto ConProducto(ObtProducto obtProducto)
+        {
+            throw new NotImplementedException();
+        }
+
+        [return: MessageParameter(Name = "Clientes")]
+        public RespClientes GetClientes(ObtInfoClientes obtenerConFecha)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        //public CompositeType GetDataUsingDataContract(CompositeType composite)
+        //{
+        //    if (composite == null)
+        //    {
+        //        throw new ArgumentNullException("composite");
+        //    }
+        //    if (composite.BoolValue)
+        //    {
+        //        composite.StringValue += "Suffix";
+        //    }
+        //    return composite;
+        //}
+
+        [return: MessageParameter(Name = "Pedido")]
+        public ResGenerarPedido resGenerarPedido(GenPedido pedido)
+        {
+            throw new NotImplementedException();
+        }
+
+        [return: MessageParameter(Name = "InfMaestra")]
+        public ResInfoMaestra resInfoMaestra(InfoMaestra Info)
+        {
+            throw new NotImplementedException();
+        }
+
+        [return: MessageParameter(Name = "CarteraTotal")]
+        public ResObtenerCarteraTotal resObtCartTotal(ObtCarTotal Info)
+        {
+            throw new NotImplementedException();
+        }
+
+        [return: MessageParameter(Name = "CarteraDef")]
+        public ResObtenerCarteraTotalDef resObtCartTotalDef(ObtCarTotalDef Info)
+        {
+            throw new NotImplementedException();
+        }
+
+        [return: MessageParameter(Name = "AnularPedido")]
+        public RestAnulPedido restAnulPedido(AnulPedido Info)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
